7bb321580484fc38ab79b6be7feef5dc4c6c66f3
jdkwer <darkconquer27@gmail.com>
2026-03-31 00:29:28 +0800

    .

--- Backend/src/Entity/User.php
+++ Backend/src/Entity/User.php
@@ -184,6 +184,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     /**
      * @see UserInterface
      */
+    #[\Deprecated]
     public function eraseCredentials(): void
     {
         // If you store any temporary, sensitive data on the user, clear it here
